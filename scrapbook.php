ed25194759af378d20547e90e6e54c3a33881a87
Ashwin Raja <ashwin.craja@gmail.com>
2013-04-28 17:04:23 -0400

    added jquery to display text on hover over an img

--- scrapbook.php
+++ scrapbook.php
@@ -6,6 +6,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	
 	<head>
+	<script src="assets/js/jquery.js"></script>	
 <!-- 		<link rel="stylesheet" type="text/css" href="styles/style.css" /> -->
 		<title>Scrapbook</title>
 	</head>
@@ -26,10 +27,12 @@
 				$q = "SELECT url FROM Photos";
 				$query = $sql->query($q);
 	
+				//TO-DO: easiest way is to stick caption in alt field
 				print("<div id='albumPhotosDiv'>");
 				while ($result = $query->fetch_assoc()) {
+					$alt = "something";
 					print("
-						<img class='albumImgImg' src='images/" . $result["url"] . "' /><br />
+						<img class='albumImgImg' alt='". $alt . "' src='images/" . $result["url"] . "' /><br />
 						<br /><br /><br />
 					");
 				}
@@ -37,5 +40,16 @@
 				$sql->close();
 			?>
 		</div>
+		<script type="text/javascript">
+			$("img").hover(
+				function(){
+					var caption = $(this).attr("alt");
+					$(this).wrap('<div id="caption_div" />');
+					$("#caption_div").append("<p>" + caption "</p>");
+				},
+				function(){
+					$(this).unwrap();
+				});
+		</script>
 	</body>
 </html>
