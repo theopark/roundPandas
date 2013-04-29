7e6a49cd53973df1e0347b6d77408f045146c007
Ashwin Raja <ashwin.craja@gmail.com>
2013-04-28 20:17:14 -0400

    repo work

---
+++ menu.php
@@ -0,0 +1,10 @@
+<div class="navigation">
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="about.php">About</a></li>
+		<li><a href="getInvolved.php">Get Involved</a></li>
+		<li><a href="events.php">Events</a></li>
+		<li><a href="opportunities.php">Opportunities</a></li>
+		<li><a href="scrapbook.php">Scrapbook</a></li>
+	</ul>
+</div>
