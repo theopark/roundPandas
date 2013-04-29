796fcddd37f263232369011493e8cf744e3beb07
Ashwin Raja <ashwin.craja@gmail.com>
2013-04-28 20:15:40 -0400

    more repo work

--- theo_php/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$DB_Username = "Round_Pandas";
-$DB_Password = "mxfzt7cb2sv";
-$DB_Name = "info230_SP13FP_Round_Pandas";
-
-?>
--- theo_php/menu.php
+++
@@ -1,8 +0,0 @@
-<ul>
-	<li><a href="index.php">home</a></li>
-	<li><a href="about.php">about</a></li>
-	<li><a href="getInvolved.php">get involved</a></li>
-	<li><a href="events.php">events</a></li>
-	<li><a href="opportunities.php">opportunities</a></li>
-	<li><a href="scrapbook.php">scrapbook</a></li>
-</ul>
